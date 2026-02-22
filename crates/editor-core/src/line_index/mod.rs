d3be34c9a8e192d33af5e6c1d9c9592d46ffd6ae
tip-aaron <qaamragudos@tip.edu.ph>
2026-02-22 17:57:38 +0000

    make line_index mods pub

--- crates/editor-core/src/line_index/mod.rs
+++ crates/editor-core/src/line_index/mod.rs
@@ -1,7 +1,6 @@
-mod btree;
-mod line_iter;
-mod line_summary;
-mod node;
-mod search_cache;
-
+pub mod btree;
+pub mod line_iter;
+pub mod line_summary;
+pub mod node;
+pub mod search_cache;
 pub const MAX_CHILDREN: usize = 16;
