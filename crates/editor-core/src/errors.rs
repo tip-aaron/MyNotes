7aee5f623690c8c895c62a426df0b33a1214c6ce
tip-aaron <qaamragudos@tip.edu.ph>
2026-02-22 17:57:16 +0000

    add cursor and errors

---
+++ crates/editor-core/src/errors.rs
@@ -0,0 +1,25 @@
+pub type TextBufferResult<T> = Result<T, TextBufferError>;
+
+pub enum TextBufferError {
+    CreationError,
+    IoError(std::io::Error),
+    ConversionError(std::num::TryFromIntError),
+    IndexOutOfBounds(usize),
+    Overflow,
+}
+
+impl From<std::io::Error> for TextBufferError {
+    fn from(value: std::io::Error) -> Self {
+        TextBufferError::IoError(value)
+    }
+}
+
+impl From<crate::enums::MathError> for TextBufferError {
+    fn from(value: crate::enums::MathError) -> Self {
+        match value {
+            crate::enums::MathError::ConversionFailed(val) => TextBufferError::ConversionError(val),
+            crate::enums::MathError::OutOfBounds(val) => TextBufferError::IndexOutOfBounds(val),
+            crate::enums::MathError::Overflow => TextBufferError::Overflow,
+        }
+    }
+}
